0eb6671709289a9d62e65c6ecd2f72903551d7c5
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-08-27 00:10:14 +0000

    feito

--- programaZOO.py
+++ programaZOO.py
@@ -9,24 +9,6 @@ class Animal:
         self.vizinhos = []  
         self.horas_alimentacao = horas_alimentacao
     
-    def adicionar_vizinho(self, vizinho):
-        if len(self.vizinhos) < 2:
-            self.vizinhos.append(vizinho)
-        else:
-            print(f"{self.nome} já tem o número máximo de vizinhos.")
-
-    def fazer_barulho(self):
-        return f"{self.nome} faz {self.barulho}!"
-
-    def se_movimentar(self):
-        return f"{self.nome} se move {self.movimento}."
-    
-    def alimentar(self):
-        return f"{self.nome} foi alimentado às {self.horas_alimentacao}."
-    
-    def __str__(self):
-        return f"{self.nome}, {self.idade} anos, {self.alimentacao}, Habitat: {self.habitat}"
-
 class Mamifero(Animal):
     def __init__(self, nome, idade, barulho, movimento, alimentacao, habitat, horas_alimentacao):
         Animal.__init__(self, nome, idade, barulho, movimento, alimentacao, habitat, horas_alimentacao)
